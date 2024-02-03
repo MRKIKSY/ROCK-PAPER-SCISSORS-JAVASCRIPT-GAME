fd506892c5a7a5ac82060ba468bba31bbdbf347b
SAMUEL AKINOLA ELUYEFA <149276078+MRKIKSY@users.noreply.github.com>
2024-02-03 01:58:36 +0000

    Update index.js

--- index.js
+++ index.js
@@ -13,20 +13,9 @@ function resetGame() {
   });
 
   // Set default images and result text
-  userResult.src = cpuResult.src = "images/rock.png";
+  userResult.src = cpuResult.src = "rock.png";
   result.textContent = "Wait...";
 }
-
-// Function to update and display the scoreboard
-function updateScoreboard(userScore, cpuScore) {
-  const scoreboard = document.querySelector(".scoreboard");
-  scoreboard.textContent = `User: ${userScore} - CPU: ${cpuScore}`;
-}
-
-// Check if there is a scoreboard in local storage, if not initialize it
-let userScore = localStorage.getItem("userScore") || 0;
-let cpuScore = localStorage.getItem("cpuScore") || 0;
-updateScoreboard(userScore, cpuScore);
 
 // Loop through each option image element
 optionImages.forEach((image, index) => {
@@ -51,7 +40,7 @@ optionImages.forEach((image, index) => {
       // Generate a random number between 0 and 2
       let randomNumber = Math.floor(Math.random() * 3);
       // Create an array of CPU image options
-      let cpuImages = ["images/rock.png", "images/paper.png", "images/scissors.png"];
+      let cpuImages = ["rock.png", "scissors.png", "paper.png"];
       // Set the CPU image to a random option from the array
       cpuResult.src = cpuImages[randomNumber];
 
@@ -62,9 +51,9 @@ optionImages.forEach((image, index) => {
 
       // Create an object with all possible outcomes
       let outcomes = {
-        RR: "Draw",
+          RR: "Draw",
         RP: "MR KIKSY",
-        RS: "User",
+        RS: "YOU",
         PP: "Draw",
         PR: "User",
         PS: "MR KIKSY",
@@ -76,14 +65,6 @@ optionImages.forEach((image, index) => {
       // Look up the outcome value based on user and CPU options
       let outComeValue = outcomes[userValue + cpuValue];
 
-      // Update the scoreboard based on the result
-      if (userValue !== cpuValue) {
-        outComeValue === "User" ? userScore++ : cpuScore++;
-        localStorage.setItem("userScore", userScore);
-        localStorage.setItem("cpuScore", cpuScore);
-        updateScoreboard(userScore, cpuScore);
-      }
-
       // Display the result
       result.textContent = userValue === cpuValue ? "Match Draw" : `${outComeValue} Won!!`;
 
@@ -94,4 +75,3 @@ optionImages.forEach((image, index) => {
     }, 2500);
   });
 });
-
