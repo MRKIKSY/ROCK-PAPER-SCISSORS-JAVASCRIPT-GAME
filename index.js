b76061c3feca610cd742f3c60d5e13f053c16c91
SAMUEL AKINOLA ELUYEFA <149276078+MRKIKSY@users.noreply.github.com>
2024-02-03 01:08:11 +0000

    Update index.js

--- index.js
+++ index.js
@@ -1,24 +1,30 @@
-// Get  to DOM elements
+// Get DOM elements
 const gameContainer = document.querySelector(".container"),
   userResult = document.querySelector(".user_result img"),
   cpuResult = document.querySelector(".cpu_result img"),
   result = document.querySelector(".result"),
   optionImages = document.querySelectorAll(".option_image");
 
+// Function to reset the game
+function resetGame() {
+  // Remove "active" class from all option images
+  optionImages.forEach((image) => {
+    image.classList.remove("active");
+  });
+
+  // Set default images and result text
+  userResult.src = cpuResult.src = "images/rock.png";
+  result.textContent = "Wait...";
+}
+
 // Loop through each option image element
 optionImages.forEach((image, index) => {
   image.addEventListener("click", (e) => {
+    // Reset the game
+    resetGame();
+
+    // Add "active" class to the clicked option image
     image.classList.add("active");
-
-    userResult.src = cpuResult.src = "images/rock.png";
-    result.textContent = "Wait...";
-
-    // Loop through each option image again
-    optionImages.forEach((image2, index2) => {
-      // If the current index doesn't match the clicked index
-      // Remove the "active" class from the other option images
-      index !== index2 && image2.classList.remove("active");
-    });
 
     gameContainer.classList.add("start");
 
@@ -45,15 +51,15 @@ optionImages.forEach((image, index) => {
 
       // Create an object with all possible outcomes
       let outcomes = {
-        RR: "DRAW",
-        RP: "CPU",
-        RS: "YOU",
-        PP: "DRAW",
-        PR: "YOU",
-        PS: "CPU",
-        SS: "DRAW",
-        SR: "CPU",
-        SP: "YOU",
+        RR: "Draw",
+        RP: "Cpu",
+        RS: "User",
+        PP: "Draw",
+        PR: "User",
+        PS: "Cpu",
+        SS: "Draw",
+        SR: "Cpu",
+        SP: "User",
       };
 
       // Look up the outcome value based on user and CPU options
@@ -61,6 +67,12 @@ optionImages.forEach((image, index) => {
 
       // Display the result
       result.textContent = userValue === cpuValue ? "Match Draw" : `${outComeValue} Won!!`;
+
+      // Reload the window after a brief delay
+      setTimeout(() => {
+        location.reload();
+      }, 2000);
     }, 2500);
   });
 });
+
