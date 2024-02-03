584664e318a26e0f765227fad27b888e42760b56
SAMUEL AKINOLA ELUYEFA <149276078+MRKIKSY@users.noreply.github.com>
2024-02-03 02:07:07 +0000

    Update index.js

--- index.js
+++ index.js
@@ -40,7 +40,7 @@ optionImages.forEach((image, index) => {
       // Generate a random number between 0 and 2
       let randomNumber = Math.floor(Math.random() * 3);
       // Create an array of CPU image options
-      let cpuImages = ["rock.png", "scissors.png", "paper.png"];
+      let cpuImages = ["rock.png", "paper.png", "scissors.png"];
       // Set the CPU image to a random option from the array
       cpuResult.src = cpuImages[randomNumber];
 
